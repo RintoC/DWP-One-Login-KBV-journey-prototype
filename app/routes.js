de54916071bfab49f8ef52c0b5096aaa3202ba9c
Athif-Shafiq <101559355+Athif-Shafiq@users.noreply.github.com>
2022-10-24 11:51:10 +0100

    Added in the 2nd route and the new skip route atm
    The new skip function route

--- app/routes.js
+++ app/routes.js
@@ -40,4 +40,42 @@ router.post('/address-lookup-post', function (req, res) {
     res.redirect('confirmation-page')
   })
 
+
+  router.post('/v2-address-lookup-post', function (req, res) {
+    const address = req.session.data['current-address']
+    if (address === 'address6') {
+      req.session.data['address-flat-number-current'] = ''
+      req.session.data['address-house-number-current'] = ''
+      req.session.data['address-house-name-current'] = 'Oxford House'
+      req.session.data['address-street-current'] = 'Oxford Row'
+      req.session.data['address-city-current'] = 'Leeds'
+    } else if (address === 'address7') {
+      req.session.data['address-flat-number-current'] = 'Office 14'
+      req.session.data['address-house-number-current'] = ''
+      req.session.data['address-house-name-current'] = ''
+      req.session.data['address-street-current'] = 'New Station St'
+      req.session.data['address-city-current'] = 'Leeds'
+    } else if (address === 'address8') {
+      req.session.data['address-flat-number-current'] = ''
+      req.session.data['address-house-number-current'] = '38'
+      req.session.data['address-house-name-current'] = ''
+      req.session.data['address-street-current'] = 'Park Square North'
+      req.session.data['address-city-current'] = 'Leeds'
+    } else if (address === 'address9') {
+      req.session.data['address-flat-number-current'] = ''
+      req.session.data['address-house-number-current'] = '41'
+      req.session.data['address-house-name-current'] = 'Lion House'
+      req.session.data['address-street-current'] = 'York Place'
+      req.session.data['address-city-current'] = 'Leeds'
+    } else if (address === 'address10') {
+      req.session.data['address-flat-number-current'] = ''
+      req.session.data['address-house-number-current'] = '1'
+      req.session.data['address-house-name-current'] = 'Whitehall Quay'
+      req.session.data['address-street-current'] = 'Whitehall Road'
+      req.session.data['address-city-current'] = 'Leeds'
+    }
+  
+    res.redirect('v2-confirmation-page')
+  })
+
 module.exports = router
